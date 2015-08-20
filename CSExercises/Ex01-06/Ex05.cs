910a543ac7cb93f397f2e51bcb3027f559403683
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-20 12:54:19 +0800

    ex5

--- CSExercises/Ex01-06/Ex05.cs
+++ CSExercises/Ex01-06/Ex05.cs
@@ -10,7 +10,14 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double A, B;
+            Console.Write("Enter A:");
+            A = Convert.ToDouble(Console.ReadLine());
+            B = Math.Sqrt(A);
+            Console.Write(" Square root Of A:");
+            Console.WriteLine("{0:0.000}", B);
+
+
         }
     }
 }
