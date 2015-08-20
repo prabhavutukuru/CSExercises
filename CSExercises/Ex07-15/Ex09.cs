dbeb5428a6a0c7168f0586ce94140ea977cc314e
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-20 15:54:28 +0800

    ex9

--- CSExercises/Ex07-15/Ex09.cs
+++ CSExercises/Ex07-15/Ex09.cs
@@ -10,6 +10,14 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            double X, y;
+            Console.WriteLine("Enter the x :");
+            X = Convert.ToDouble(Console.ReadLine());
+            //  y = 2 *(X * X) - 4 * X + 3;
+            y =  2* (Math.Pow(X, 2)) - 4 * X + 3;
+            Console.WriteLine(y);
+
+
             
         }
     }
