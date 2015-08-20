f53a1e66c16ae33ab6dddc380692681d0c513e77
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-20 14:32:35 +0800

    ex7

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,17 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            int s;
+            double h, t;
+
+            Console.WriteLine("enter salary:");
+            s = Convert.ToInt32(Console.ReadLine());
+           // h = Convert.ToDouble(Console.ReadLine());
+           // t = Convert.ToDouble(Console.ReadLine());
+            double TS = s + (0.1) * s + (0.03) * s;
+            Console.WriteLine(TS);
+
+
         }
     }
 }
