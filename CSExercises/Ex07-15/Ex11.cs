646e09bf2a91143577ab721a08950234b140bf8a
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-21 10:25:15 +0800

    ex11

--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -10,6 +10,16 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            double M, D, TF;
+            Console.WriteLine("Enter the distance : D");
+            D = Convert.ToDouble(Console.ReadLine());
+            M= 2.40;
+           // D = Convert.ToDouble (Console.ReadLine());
+           
+
+            TF = M + D * 0.4;
+            
+            Console.WriteLine("Total Fair is : {0:0.00}", TF);
             
         }
     }
