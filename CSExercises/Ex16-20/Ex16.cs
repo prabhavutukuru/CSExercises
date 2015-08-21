68c1cef609880b9a7cc18e763bb5ef6984e971a2
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-21 15:25:50 +0800

    ex16

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,9 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
+            string str;
+            char gen;
+            Console.WriteLine("Enter the Name");
+            str =(string) Console.ReadLine(); 
+            Console.WriteLine(" Gender");
+            gen=(char)Console.Read(); 
+           if (gen=='M')
+            { 
+                Console.WriteLine("Good Morning Mr" + str);
+            }
+            else
+                Console.WriteLine("Goodmorning Ms " + str);
+
+
+
+
+
             
         }
     }
