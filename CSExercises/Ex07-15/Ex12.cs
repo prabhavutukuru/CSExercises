0d7e3c1a1a9b2a5de90b12c67746ae61290f350b
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-21 11:01:25 +0800

    ex12

--- CSExercises/Ex07-15/Ex12.cs
+++ CSExercises/Ex07-15/Ex12.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double TF,TF1;
+
+            Console.WriteLine("Enter the value:");
+            TF = Convert.ToDouble(Console.ReadLine());
+            TF1 = Math.Round(TF,1);
+            Console.WriteLine(TF1);
         }
     }
 }
