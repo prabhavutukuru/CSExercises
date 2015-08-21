bf9b7a6cc0bf0cb1bc06dda000bd440bd0c1cbf8
vutukuruprabha <vutukuprabha@yahoo.co.in>
2015-08-21 11:46:03 +0800

    ex14

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double TF, TF1;
+
+            Console.WriteLine("Enter the value:");
+            TF = Convert.ToDouble(Console.ReadLine());
+            TF1 = Math.Ceiling((TF*10)/10);
+            Console.WriteLine(TF1);
         }
     }
 }
--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,23 @@
+using System;
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            double a, b, c, s,A ;
+            Console.WriteLine ("Enter the value A:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value B:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value C:");
+            c = Convert.ToDouble(Console.ReadLine());
+            s = (a + b + c) / 2;
+            A = Math.Sqrt( s* (s - a)*(s - b)*(s - c));
+            Console.Write("Value of A is ");
+            Console.WriteLine(A);
+
+
         }
     }
 }
